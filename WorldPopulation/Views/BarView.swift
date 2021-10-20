6d44c00e741e83ee8befd064120b69b28a5c2097
AlmadelX <almadel@almadelx.ru>
2021-10-20 21:41:08 +0500

    Final version

--- WorldPopulation/Views/BarView.swift
+++ WorldPopulation/Views/BarView.swift
@@ -1,6 +1,8 @@
 import SwiftUI
 
 struct BarView: View {
+    @State private var isVisible = false
+    
     let colors = [Color("DarkGreen"), Color("LightGreen")]
     
     var body: some View {
@@ -10,7 +12,13 @@ struct BarView: View {
             endPoint: .bottomTrailing
         )
             .cornerRadius(5.0)
-            .transition(.move(edge: .leading))
+            .opacity(isVisible ? 1.0 : 0.0)
+            .scaleEffect(isVisible ? 1.0 : 0.1)
+            .onAppear {
+                withAnimation(.spring(response: 0.4, dampingFraction: 0.2, blendDuration: 0.1)) {
+                    self.isVisible.toggle()
+                }
+            }
     }
 }
 
